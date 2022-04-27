b41a881b3a5730ad1f8ae316213eef4703a32f4f
Anton <anton@de1337ed.ru>
2022-04-27 11:46:59 +0300

    1. Added possibility to obtain a fleet list if "Aircraft type" field is empty 2. Beautified main page (but not enough)

---
+++ getFleet.py
@@ -0,0 +1,15 @@
+import requests
+import bs4
+
+
+def get_fleet(airline_code: str) -> list:
+    """Function is used to get list of Aircraft REG numbers of plane model noticed last month by certain airline"""
+    # getting raw html from flightradar
+    header = {'User-Agent': 'PostmanRuntime/7.29.0'}
+    raw_aircraft_data = requests.get('https://www.flightradar24.com/data/airlines/' + airline_code + '/fleet',
+                                     headers=header).text
+    # parsing HTML
+    fleet_soup = bs4.BeautifulSoup(raw_aircraft_data, 'html.parser')
+    raw_data = fleet_soup.find_all('dt')[1:]
+    fleet = list(map(lambda x: str(x.findChild("div")).lstrip('<div>').rstrip('</div>'), raw_data))
+    return fleet
--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from getAircraftRegs import get_regs
 from getAircraftHistory import get_history
+from getFleet import get_fleet
 from flask import Flask, render_template, request
 from functools import reduce
 import operator
@@ -13,7 +14,14 @@ def index():
     if request.method == "POST":
         # reading inputs
         airline_code = request.form.get("airline")
-        aircraft_type = request.form.get("aircraft_type ")
+        aircraft_type = request.form.get("aircraft_type ").upper()
+        if aircraft_type == '':
+            fleet = get_fleet(airline_code)
+            fleet_string = '''Okay, here's the list of aircraft types for "''' \
+                           + airline_code.upper() + '" airline: ' + ', '.join(fleet)
+            return render_template("index.html",
+                                   FleetList=fleet_string,
+                                   LinkText="Enter aircraft model to get link")
         # parsing reg numbers of aircrafts
         planes = get_regs(airline_code, aircraft_type)
         # constructing list of lists of routes
@@ -21,11 +29,12 @@ def index():
         for aircraft in planes:
             total_routes.append(get_history(aircraft))
         total_routes = set(reduce(operator.iconcat, total_routes, []))  # list of unique routes among all aircrafts
-        link = "http://www.gcmap.com/mapui?P=" + ",".join(total_routes)  # generating GCMap link
+        link = "http://www.gcmap.com/mapui?P=" + ','.join(total_routes)  # generating GCMap link
         return render_template("index.html",
                                AircraftList=", ".join(planes),
                                RoutesList=", ".join(total_routes),
-                               GCMapLink=link)
+                               GCMapLink=link,
+                               LinkText="Click here to plot map")
 
     return render_template("index.html")
 
