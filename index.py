0028ee0a063a466a42c956993c7598f8bfad0982
anton <anton@de1337ed.ru>
2023-03-12 16:32:47 +0300

    Implemented basic backend APIs

--- index.py
+++ index.py
@@ -10,7 +10,6 @@ from getFleet import get_fleet
 from datetime import datetime
 from getAirlinesList import get_airlines
 
-# app = Flask(__name__, template_folder='templates')
 app = FastAPI()
 
 
@@ -34,51 +33,21 @@ async def get_aircrafts_regs(airline_code: str, aircraft_code: str):
     return get_regs(airline_code, aircraft_code)
 
 
-@app.get('/airlines/{airline_code}/fleet/{aircraft_code}/{routes}')
+@app.get('/airlines/{airline_code}/fleet/{aircraft_code}/routes/')
 async def get_routes(airline_code: str, aircraft_code: str):
-    return get_history(airline_code, aircraft_code)
+    planes = get_regs(airline_code, aircraft_code)
+    print(planes)
+    # constructing list of lists of routes
+    total_routes = []
+    for aircraft in planes:
+        total_routes.append(get_history(aircraft))
+    total_routes = set(reduce(operator.iconcat, total_routes, []))  # list of unique routes among all aircrafts
+    link = "http://www.gcmap.com/mapui?P=" + ','.join(total_routes)  # generating GCMap link
+    print(link)
+    return total_routes
 
-# @app.route('/', methods=["GET", "POST"])
-# def index():
-    # airlines = get_airlines()
-#    airlines = json.load(open("airlines.json"))
-#    airline_titles = list(airlines.keys())
+
 #    raw_update_timestamp = os.path.getmtime("airlines.json")
 #    list_timestamp = datetime.fromtimestamp(raw_update_timestamp).strftime('%Y-%m-%d %H:%M:%S')
-#    if request.method == "POST":
-        # reading inputs
-#        chosen_airline = request.form.get("airline")
-#        airline_link = airlines[chosen_airline]
-#        aircraft_type = request.form.get("aircraft_type")
-#        fleet = get_fleet(airline_link)
-#        if aircraft_type is None:
-#            return render_template("pre-result.html",
-#                                   chosen_airline=chosen_airline,
-#                                   airline_titles=airline_titles,
-#                                   fleet=fleet,
-#                                   LinkText="Enter aircraft model to get link")
-        # parsing reg numbers of aircrafts
-#        planes = get_regs(airline_link, aircraft_type)
-        # constructing list of lists of routes
-#        total_routes = []
-#        for aircraft in planes:
-#            total_routes.append(get_history(aircraft))
-#        total_routes = set(reduce(operator.iconcat, total_routes, []))  # list of unique routes among all aircrafts
+
 #        link = "http://www.gcmap.com/mapui?P=" + ','.join(total_routes)  # generating GCMap link
-#        return render_template("result.html",
-#                               chosen_airline=chosen_airline,
-#                               airline_titles=airline_titles,
-#                               AircraftList=", ".join(planes),
-#                               chosen_aircraft=aircraft_type,
-#                               RoutesList=", ".join(total_routes),
-#                               GCMapLink=link,
-#                               fleet=fleet,
-#                               LinkText="Click here to plot map")
-
-#    return render_template("index.html",
-#                           airline_titles=airline_titles,
-#                           allist_timestamp=list_timestamp)
-
-
-# if __name__ == '__main__':
-#    app.run(debug=True)
